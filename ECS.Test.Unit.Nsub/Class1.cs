009c36a694080da9f78a34ce4cf314ae16623d87
William Morgensol <201606569@post.au.dk>
2019-02-19 15:24:10 +0100

    Added T==HighTreshold and T>HighThreshold

--- ECS.Test.Unit.Nsub/Class1.cs
+++ ECS.Test.Unit.Nsub/Class1.cs
@@ -1,4 +1,6 @@
-﻿using NSubstitute;
+﻿using System;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using NUnit;
 using NUnit.Framework;
 
@@ -20,5 +22,60 @@ namespace ECS.Test.Unit.Nsub
             _window = Substitute.For<IWindow>();
             _uut = new ECS(_tempSensor,_heater, _window,-10,25);
         }
+
+        [Test]
+        public void Thresholds_ValidUpperTemperatureThresholdSet_NoExceptionsThrown()
+        {
+            Assert.That(()=>_uut.UpperTemperatureThreshold=20,Throws.Nothing);
+
+        }
+        [Test]
+        public void Thresholds_ValidLowerTemperatureThresholdSet_NoExceptionsThrown()
+        {
+            // Check that it doesn't throw 
+            // First parameter is a lambda expression, implicitly acting
+            Assert.That(() => { _uut.LowerTemperatureThreshold = 20; }, Throws.Nothing);
+        }
+
+        #region T==High
+
+        [Test]
+        public void Regulate_TempIsAtUpperThreshold_HeaterIsTurnedOff()
+        {
+            _tempSensor.GetTemp().Returns(_uut.UpperTemperatureThreshold);
+            _uut.Regulate();
+            _heater.Received(1).TurnOff();
+
+        }
+
+        [Test]
+        public void Regulate_TempIsAtUpperThreshold_WindowIsClosed()
+        {
+            _tempSensor.GetTemp().Returns(_uut.UpperTemperatureThreshold);
+            _uut.Regulate();
+            _window.Received(1).Close();
+        }
+
+            #endregion
+        #region T>High
+        [Test]
+        public void Regulate_TempIsAboveUpperThreshold_HeaterIsTurnedOff()
+        {
+            // Setup the stub with desired response
+            _tempSensor.GetTemp().Returns(27);
+            _uut.Regulate();
+            _heater.Received(1).TurnOff();
+        }
+        [Test]
+        public void Regulate_TempIsAboveUpperThreshold_WindowIsOpened()
+        {
+            // Setup the stub with desired response
+            _tempSensor.GetTemp().Returns(27);
+            _uut.Regulate();
+            _window.Received(1).Open();
+        }
+
+
+        #endregion
     }
 }
