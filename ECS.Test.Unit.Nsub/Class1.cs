2b78643b29e44b569d7c45c758959edb870f3375
Welle77 <36986182+Welle77@users.noreply.github.com>
2019-02-19 15:25:50 +0100

    Min Del af ECS

--- ECS.Test.Unit.Nsub/Class1.cs
+++ ECS.Test.Unit.Nsub/Class1.cs
@@ -26,11 +26,54 @@ namespace ECS.Test.Unit.Nsub
         [Test]
         public void ThreshHolds_ValidUpperTemperatureThreshHoldSet_NoExceptionsThrown()
         {
-            _uut
-                .When()
-            
+            Assert.That(() => _uut.UpperTemperatureThreshold = 25, Throws.Nothing);
         }
 
+        [Test]
+        public void Regulate_TempIOsLow_HeaterIsTurnedOn()
+        {
+            _tempSensor.GetTemp().Returns(_uut.LowerTemperatureThreshold - 10);
+            _uut.Regulate();
+            _heater.Received(1).TurnOn();
+        }
 
+        [Test]
+        public void Regulate_TempIsLow_WindowIsClosed()
+        {
+            _tempSensor.GetTemp().Returns(_uut.LowerTemperatureThreshold - 10);
+            _uut.Regulate();
+            _window.Received(1).Close();
+        }
+
+        [Test]
+        public void Regulate_TempIsAtLowerThresh_HeaterIsTurnedOff()
+        {
+            _tempSensor.GetTemp().Returns(_uut.LowerTemperatureThreshold);
+            _uut.Regulate();
+            _heater.Received(1).TurnOff();
+        }
+
+        [Test]
+        public void RegulateTempIsAtLowerThresh_WindowIsClosed()
+        {
+            _tempSensor.GetTemp().Returns(_uut.LowerTemperatureThreshold);
+            _uut.Regulate();
+            _window.Received(1).Close();
+        }
+
+        [Test]
+        public void Regulate_TempIsBetweenLowerAndUpperThresh_HeaterIsTurnedOff()
+        {
+            _tempSensor.GetTemp().Returns(_uut.UpperTemperatureThreshold - 5);
+            _uut.Regulate();
+            _heater.Received(0).TurnOn();
+        }
+        [Test]
+        public void Regulate_TempIsBetweenLoweAndUpperThresh_WindowIsNotOpen()
+        {
+            _tempSensor.GetTemp().Returns(_uut.UpperTemperatureThreshold - 5);
+            _uut.Regulate();
+            _window.Received(0).Open();
+        }
     }
 }
